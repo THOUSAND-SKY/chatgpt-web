0e1948573e5d5189893c92d1ad9981778683109e
THOUSAND-SKY <104664691+THOUSAND-SKY@users.noreply.github.com>
2023-12-06 08:16:48 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 import './app.scss'
 import App from './App.svelte'
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(() => {}).catch(() => {})
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
 const app = new App({
   target: document.getElementById('app') as HTMLElement
