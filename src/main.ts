e132bba0747718b7c9b56fa72c99f21308e64378
THOUSAND-SKY <104664691+THOUSAND-SKY@users.noreply.github.com>
2023-12-06 08:07:04 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 import './app.scss'
 import App from './App.svelte'
-
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+}
 const app = new App({
   target: document.getElementById('app') as HTMLElement
 })
