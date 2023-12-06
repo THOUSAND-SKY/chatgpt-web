e2ef94bd71af7c56b7b4cd965832e8bfe6e58bf9
Ossi Karki <g4gossi@gmail.com>
2023-12-06 11:10:32 +0100

    fix manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import dsv from '@rollup/plugin-dsv'
 import purgecss from '@fullhuman/postcss-purgecss'
-import { join } from 'node:path'
-import { buildSync } from 'esbuild'
 
-const plugins = [svelte(), dsv(), {
-  apply: 'build',
-  enforce: 'post',
-  transformIndexHtml () {
-    buildSync({
-      minify: true,
-      bundle: true,
-      entryPoints: [join(process.cwd(), 'src/sw.js')],
-      outfile: join(process.cwd(), 'dist', 'assets', 'sw.js')
-    })
-  }
-}, {
-  apply: 'build',
-  enforce: 'post',
-  transformIndexHtml () {
-    buildSync({
-      minify: true,
-      bundle: true,
-      entryPoints: [join(process.cwd(), 'src/assets/manifest.json')],
-      outfile: join(process.cwd(), 'dist', 'assets', 'manifest.json')
-    })
-  }
-}]
+const plugins = [svelte(), dsv()]
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
